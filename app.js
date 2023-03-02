fa11d03865cc131c0974782b41fffffdd54f889c
Alchemi1963 <alchemi1999@hotmail.com>
2023-03-02 16:39:39 +0100

    Moved CSS files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
@@ -5,8 +6,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 
 //laad cocktails
-const sterk = require("./script/drank");
-const cocktails = require("./script/cocktails");
+const sterk = require("/script/drank");
+const cocktails = require("/script/cocktails");
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(helmet());
 
 app.get('/new', (req, res) => {
     res.redirect("/new.html")
-    //res.sendFile(path.join(__dirname, './public/new.html'));
+    //res.sendFile(path.join(__dirname, '/public/new.html'));
 });
 app.post("/new", (req, res) => {
     console.log(req.body);
@@ -29,9 +30,6 @@ app.get("/login", (req, res) => {
     res.setHeader("Set-Cookie", 'bolk-oath-access-token=true');
     res.redirect("/");
 });
-app.post("/login", (req, res) => {
-    res.setHeader("Set-Cookie", 'bolk-oath-access-token=true');
-    res.redirect("/");
-});
+
 
 app.listen(3000)
